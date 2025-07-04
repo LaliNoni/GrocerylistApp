eb32fc47aba8c360ee35f71039bae661fe6d690c
Liad-9 <liadsamara@gmail.com>
2025-07-04 19:11:21 +0300

    meantime app

--- app/src/main/java/com/example/grocerylistapp/HomePageFragment.kt
+++ app/src/main/java/com/example/grocerylistapp/HomePageFragment.kt
@@ -1,40 +1,30 @@
 package com.example.grocerylistapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-
 
 class HomePageFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_page, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
-        bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_grocery -> {
-                    // TODO: Navigate to Grocery screen
-                    true
-                }
-                R.id.nav_search -> {
-                    // TODO: Navigate to Search screen
-                    true
-                }
-                R.id.nav_add -> {
-                    // TODO: Navigate to Add screen
-                    true
-                }
-                R.id.nav_profile -> {
-                    // TODO: Navigate to Profile screen
-                    true
-                }
-                else -> false
-            }
-        }
+        val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val createAccountButton = view.findViewById<Button>(R.id.create_account_button)
+        val loginTextView = view.findViewById<TextView>(R.id.text_login)
+
     }
 }
--- app/src/main/java/com/example/grocerylistapp/MainActivity.kt
+++ app/src/main/java/com/example/grocerylistapp/MainActivity.kt
@@ -1,20 +1,64 @@
 package com.example.grocerylistapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, HomePageFragment())
+            .commit()
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+           val selectedFragment = when(item.itemId) {
+               R.id.nav_grocery -> Grocery_lists_Fragment()
+               R.id.nav_search -> SearchFragment() // Replace with your fragment class
+               R.id.nav_add -> Add_list_Fragment() // Replace with your fragment class
+               R.id.nav_profile -> ProfileFragment() // Replace with your fragment class
+               else -> null
+            }
+            selectedFragment?.let {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, it)
+                    .commit()
+                true
+            } ?: false
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_notifications -> {
+                // TODO: Handle notifications click
+                true
+            }
+            R.id.action_settings -> {
+                // TODO: Handle settings click
+                true
+            }
+            R.id.action_logout -> {
+                // TODO: Handle logout click
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
